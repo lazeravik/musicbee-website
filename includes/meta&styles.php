140a243fe4ad5a74fa4375229c7968557e7f62be
Avik <avik.avikbiswas@gmail.com>
2016-02-12 22:25:41 +0530

    added css

--- includes/meta&styles.php
+++ includes/meta&styles.php
@@ -3,6 +3,6 @@
 <!-- responsive mobile deivicew support -->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Load external stylesheets and scripts -->
-	<link rel="stylesheet" type="text/css" href="<?php echo $siteUrl; ?>styles/MusicBeeStyle.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo $siteUrl; ?>styles/mb_main.css">
 	<link rel="stylesheet" href="<?php echo $siteUrl; ?>styles/font-awesome.min.css">
 	<link rel="alternate" href="<?php echo $siteUrl; ?>rss" title="MusicBee updates" type="application/rss+xml" />
