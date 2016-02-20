2be1bb8a30a02ea8c396be832442f1aa6460d536
Avik <avik.avikbiswas@gmail.com>
2016-02-20 15:29:19 +0530

    added grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            sass: {
+                files: 'styles/scss/**/*.scss',
+                tasks: [
+                    'sass',
+                    'autoprefixer'
+                ]
+            }
+        },
+        uglify: {
+            build: {
+                src: 'js/*.js',
+                dest: 'js/build/main.min.js'
+            }
+        },
+        sass: {
+            options: {
+                style: 'compressed',
+            },
+            dist: {
+                files: {
+                    'styles/src/mb_main.css': 'styles/scss/mb_main.scss'
+                }
+            }
+        },
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions', 'ie 9', 'ie 10', '> 1%']
+            },
+            main: {
+                expand: true,
+                flatten: true,
+                src: 'styles/src/*.css',
+                dest: 'styles/dist/'
+            }
+        },
+         // browserSync: {
+             // default_options: {
+                 // bsFiles: {
+                     // src: [
+                         // "styles/dist/*.css",
+                         // "*.php",
+                     // ]
+                 // },
+                 // options: {
+                     // watchTask: true,
+                     // proxy: "localhost",
+                     // online: false,
+                 // }
+             // }
+         // }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    //grunt.loadNpmTasks('grunt-contrib-uglify');
+    //grunt.loadNpmTasks('grunt-sass');
+    //grunt.loadNpmTasks('grunt-autoprefixer');
+    //grunt.loadNpmTasks('grunt-contrib-watch');
+    //grunt.loadNpmTasks('grunt-browser-sync');
+
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    // Default task(s).
+    // grunt.registerTask('default', ['browserSync', 'watch']);
+    grunt.registerTask('default', ['sass', 'autoprefixer', 'watch']);
+
+};
